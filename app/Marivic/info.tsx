8fb7721e480ea466f50e65228d109db0d6942995
Marivic Alcantara <marivicarcaalcantara@gmail.com>
2025-09-16 19:54:43 +0800

    added marivic page

---
+++ app/Marivic/info.tsx
@@ -0,0 +1,5 @@
+export default function Info() {
+  return( 
+  <p>mapsek aku</p>
+  )
+}
---
+++ app/Marivic/page.tsx
@@ -0,0 +1,11 @@
+import Info from "./info";
+
+export default function Page() {
+  return(
+    <div>
+  <h1 className="text-3xl font-bold">Marivic Alcantara</h1>
+
+  <Info/>
+  </div>
+);
+}
