3412bcb735c689f623592a0a7b0598eaa0f58e99
Marivic Alcantara <marivicarcaalcantara@gmail.com>
2025-11-18 18:34:03 +0800

    static and dynamic rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,6 +8,10 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 // --- Sample revenue data (used for your Revenue Chart) ---
 export async function fetchRevenue() {
+   console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log('Data fetch completed after 3 seconds.');
   return [
     { month: 'January', revenue: 12000 },
     { month: 'February', revenue: 8000 },
